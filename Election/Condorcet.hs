93d3cb8bc6cd3d1804adce9854c9f5456d31fbe2
Murat Kasimov <iokasimov.m@gmail.com>
2021-08-19 20:38:29 +0300

    Prototype an actual function for voting

--- Election/Condorcet.hs
+++ Election/Condorcet.hs
@@ -1,1 +1,8 @@
 module Election.Condorcet where
+
+import "base" Data.Function ((.), (&))
+import "base" Data.Int (Int)
+import "lens" Control.Lens (element, (.~))
+
+vote :: [[Int]] -> Int -> Int -> [[Int]]
+vote prefers voter votes = prefers & element voter . element votes .~ 1
